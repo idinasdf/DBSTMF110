8f406ccefabbf731165d0d47ec9baf70904bc762
idinasdf <nitirujidin@gmail.com>
2026-09-25 23:01:24 +0700

    Add files via upload

---
+++ env.js
@@ -0,0 +1,6 @@
+// ไฟล์นี้ถูกสร้างอัตโนมัติจาก .env โดย generate-env.js
+// อย่าแก้ไขตรงนี้ / อย่า commit ขึ้น git
+window.__ENV__ = {
+  SUPABASE_URL: "https://lqmnccgbixniiaejdrtb.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_sOLRpt3Tzv1R0SHvNH9hkA_mbG_2wPM"
+};
